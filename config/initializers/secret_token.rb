661f412adbf3691679c0e620dc79e4ab46893c8b
Geren White <gtwhite90@gmail.com>
2013-12-25 20:19:32 -0500

    Initial commit.  Brand new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CCTVViewer::Application.config.secret_key_base = '2c104698ba082943d36570fd5be9fc92d2794c32aea2b82f1a1e5f1f252571176bead8bc851d638448abb53e8a5a251c79ef09fe794108712bf8bc3f965a362e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CCTVViewer::Application.config.session_store :cookie_store, key: '_CCTVViewer_session'
